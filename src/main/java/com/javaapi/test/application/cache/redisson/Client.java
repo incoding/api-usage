d8d068665a87f2fb7ac4035b9913409816b68ad1
kun.wang1 <kun.wang1@corp.email.com>
2018-11-06 19:07:38 +0800

    rate limit & redisson

---
+++ src/main/java/com/javaapi/test/application/cache/redisson/Client.java
@@ -0,0 +1,7 @@
+package com.javaapi.test.application.cache.redisson;
+
+/**
+ * Created by user on 2018/11/4
+ */
+public class Client {
+}
---
+++ src/main/java/com/javaapi/test/application/cache/redisson/RedissonImpl.java
@@ -0,0 +1,7 @@
+package com.javaapi.test.application.cache.redisson;
+
+/**
+ * // redisson lock 实现
+ */
+public class RedissonImpl {
+}
