9dd650261929e403bf0bf6eb289e4eb28353d8c7
王昆 <kun.wang1@ly.com>
2019-05-19 19:01:52 +0800

    client catch finally

---
+++ src/main/java/com/javaapi/test/buisness/pattern/context/AppContext.java
@@ -0,0 +1,8 @@
+package com.javaapi.test.buisness.pattern.context;
+
+/**
+ * Created by user on 2019/5/5
+ */
+public class AppContext {
+
+}
---
+++ src/main/java/com/javaapi/test/test/type/String/testString/StringBuilderTest.java
@@ -0,0 +1,24 @@
+package com.javaapi.test.test.type.String.testString;
+
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONObject;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.Test;
+
+/**
+ * Created by user on 2019/5/15
+ */
+@Slf4j
+public class StringBuilderTest {
+    @Test
+    public void test() {
+        Long version = null;
+        String id = null;
+        StringBuilder stringBuilder = new StringBuilder();
+        String s = stringBuilder.append(version).append(id).toString();
+        System.out.println("s = " + s);
+        log.info("输出为 {} ,{}", id, version);
+        StringBuilder stringBuilder1 = new StringBuilder();
+        System.out.println("s = " + stringBuilder1.toString().length());
+    }
+}
