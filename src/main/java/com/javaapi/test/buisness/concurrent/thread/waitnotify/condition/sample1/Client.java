d99060d7264bd98d8f158bb34e2c0c600b5c4092
王昆 <kun.wang1@ly.com>
2021-09-01 17:50:38 +0800

    before signal invoke  ,current thread must associate with lock

--- src/main/java/com/javaapi/test/buisness/concurrent/thread/waitnotify/condition/sample1/Client.java
+++ src/main/java/com/javaapi/test/buisness/concurrent/thread/waitnotify/condition/sample1/Client.java
@@ -7,6 +7,10 @@ import java.util.concurrent.locks.ReentrantLock;
 
 public class Client {
 
+
+    /**
+     * 当前线程收到signal后，然后获取到锁后才会从await返回
+     */
     @Test
     public void test(){
         ReentrantLock reentrantLock = new ReentrantLock();
@@ -16,6 +20,23 @@ public class Client {
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
+
+
+    }
+
+    /**
+     * signal 唤醒其他线程,但是前提是当前线程要获取到锁
+     */
+    @Test
+    public void testSignal() {
+        ReentrantLock reentrantLock = new ReentrantLock();
+        Condition condition = reentrantLock.newCondition();
+        try {
+            condition.signal();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
 
     }
 }
