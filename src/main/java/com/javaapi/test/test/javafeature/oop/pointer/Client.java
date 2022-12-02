f3ab5ae63e058fa8c53594adc5ccd4068ef62fc4
王昆 <kun.wang1@ly.com>
2022-12-02 08:59:42 +0800

    java theory

--- src/main/java/com/javaapi/test/test/javafeature/oop/pointer/Client.java
+++ src/main/java/com/javaapi/test/test/javafeature/oop/pointer/Client.java
@@ -4,6 +4,10 @@ import com.javaapi.test.test.javafeature.oop.miscell.Person;
 import org.junit.Test;
 
 public class Client {
+
+    /**
+     * 教学 引用是基于复制的 case1
+     **/
     @Test
     public void test() {
         Person a = new Person();
@@ -18,5 +22,23 @@ public class Client {
         a = c;
         System.out.println("a.getName() = " + a.getName());
         System.out.println("b.getName() = " + b.getName());
+    }
+
+    /**
+     * 教学  引用是基于复制的 case2
+     **/
+    @Test
+    public void test2() {
+        Person a = new Person();
+        a.setName("name a");
+        changeB(a);
+        System.out.println("a out of  method is " + a.getName());
+    }
+
+    private void changeB(Person a) {
+        Person c = new Person();
+        c.setName("name c");
+        a = c;
+        System.out.println("a in method is " + a.getName());
     }
 }
