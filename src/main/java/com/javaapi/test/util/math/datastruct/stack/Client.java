67697e24461cad5a2ac507767e5022e37ee99c1e
王昆 <kun.wang1@ly.com>
2022-12-10 22:29:51 +0800

    backteace

--- src/main/java/com/javaapi/test/util/math/datastruct/stack/Client.java
+++ src/main/java/com/javaapi/test/util/math/datastruct/stack/Client.java
@@ -1,2 +1,41 @@
-package com.javaapi.test.util.math.datastruct.stack;public class Client {
+package com.javaapi.test.util.math.datastruct.stack;
+
+import org.junit.Test;
+
+import java.util.ArrayDeque;
+import java.util.Deque;
+
+public class Client {
+
+    @Test
+    public void test() {
+        boolean valid = new Client().isValid("(()()((())))");
+        System.out.println("valid = " + valid);
+    }
+
+
+    @Test
+    public void test2() {
+        boolean valid = new Client().isValid("(())())(()");
+        System.out.println("valid = " + valid);
+    }
+
+
+    public boolean isValid(String s) {
+        Deque<Character> stack = new ArrayDeque<>();
+
+        for (char c : s.toCharArray()) {
+            if (c == '(') {
+                stack.push(c);
+            } else {
+                if (stack.isEmpty()) {
+                    return false;
+                } else {
+                    stack.pop();
+                }
+            }
+        }
+        return stack.isEmpty();
+    }
+
 }
