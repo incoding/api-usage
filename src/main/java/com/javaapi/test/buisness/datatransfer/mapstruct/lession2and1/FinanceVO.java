1dc2ca0b67aa1e4f514ec3f2d4c268cb58b8f859
王昆 <kun.wang1@ly.com>
2023-01-04 16:55:50 +0800

    mapstruct custom method

---
+++ src/main/java/com/javaapi/test/buisness/datatransfer/mapstruct/lession2and1/FinanceVO.java
@@ -0,0 +1,13 @@
+package com.javaapi.test.buisness.datatransfer.mapstruct.lession2and1;
+
+import lombok.Data;
+
+/**
+ * Created by user on 2020/10/2.
+ */
+@Data
+public class FinanceVO {
+
+    private String author;
+
+}
