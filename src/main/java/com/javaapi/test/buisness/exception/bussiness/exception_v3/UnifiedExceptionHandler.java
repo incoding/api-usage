1fd8bece4fa1a37b5ce1d8967321db4cd8979b4d
王昆 <kun.wang1@ly.com>
2023-03-30 11:35:50 +0800

    exception demo2

--- src/main/java/com/javaapi/test/buisness/exception/bussiness/exception_v3/UnifiedExceptionHandler.java
+++ src/main/java/com/javaapi/test/buisness/exception/bussiness/exception_v3/UnifiedExceptionHandler.java
@@ -1,17 +1,34 @@
 package com.javaapi.test.buisness.exception.bussiness.exception_v3;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.ConversionNotSupportedException;
+import org.springframework.beans.TypeMismatchException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnWebApplication;
+import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.http.converter.HttpMessageNotWritableException;
 import org.springframework.stereotype.Component;
+import org.springframework.web.HttpMediaTypeNotAcceptableException;
+import org.springframework.web.HttpMediaTypeNotSupportedException;
+import org.springframework.web.HttpRequestMethodNotSupportedException;
+import org.springframework.web.bind.MissingPathVariableException;
+import org.springframework.web.bind.MissingServletRequestParameterException;
+import org.springframework.web.bind.ServletRequestBindingException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
-//
-//@Slf4j
-//@Component
-//@ControllerAdvice
-//@ConditionalOnWebApplication
-//@ConditionalOnMissingBean(UnifiedExceptionHandler.class)
-//public class UnifiedExceptionHandler {
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.context.request.async.AsyncRequestTimeoutException;
+import org.springframework.web.multipart.support.MissingServletRequestPartException;
+import org.springframework.web.servlet.NoHandlerFoundException;
+
+@Slf4j
+@Component
+@ControllerAdvice
+@ConditionalOnWebApplication
+@ConditionalOnMissingBean(UnifiedExceptionHandler.class)
+public class UnifiedExceptionHandler {
 //    /**
 //     * 生产环境
 //     */
@@ -187,5 +204,5 @@ import org.springframework.web.bind.annotation.ControllerAdvice;
 //
 //        return new ErrorResponse(CommonResponseEnum.SERVER_ERROR.getCode(), e.getMessage());
 //    }
-//
-//}
+
+}
