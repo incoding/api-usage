638187df746b45bd799cb2d8de6f92aebbff2a94
王昆 <kun.wang1@ly.com>
2023-09-18 21:01:23 +0800

    test spel

---
+++ src/test/java/com/javaapi/test/application/spel/TestSpel.java
@@ -0,0 +1,28 @@
+package com.javaapi.test.application.spel;
+
+import org.junit.Test;
+import org.springframework.expression.Expression;
+import org.springframework.expression.spel.standard.SpelExpressionParser;
+
+public class TestSpel {
+    @Test
+    public void test() {
+        SpelExpressionParser parser = new SpelExpressionParser();
+        Expression expression = parser.parseExpression("#root.purchaseName");
+        Order order = new Order();
+        order.setPurchaseName("张三");
+        System.out.println(expression.getValue(order));
+    }
+
+    public class Order {
+        public String purchaseName;
+
+        public String getPurchaseName() {
+            return purchaseName;
+        }
+
+        public void setPurchaseName(String purchaseName) {
+            this.purchaseName = purchaseName;
+        }
+    }
+}
