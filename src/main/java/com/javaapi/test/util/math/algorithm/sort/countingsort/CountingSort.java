81b27dee0e3819347e84ebd6f5db7ce523f271eb
王昆 <kun.wang1@ly.com>
2024-04-15 09:16:01 +0800

    计数排序

---
+++ src/main/java/com/javaapi/test/util/math/algorithm/sort/countingsort/CountingSort.java
@@ -0,0 +1,41 @@
+package com.javaapi.test.util.math.algorithm.sort.countingsort;
+
+import org.junit.Test;
+
+import java.util.Arrays;
+
+public class CountingSort {
+
+    @Test
+    public void sortArray() {
+        int[] a = {2, 3, 1, 9, 5, 6, 6, 6, 6};
+        int[] result = new CountingSort().sortArray(a);
+        System.out.println("temp = " + Arrays.toString(result));
+
+    }
+
+    public int[] sortArray(int[] nums) {
+        int min = Integer.MAX_VALUE;
+        int max = Integer.MIN_VALUE;
+        // 获取最大最小值
+        for (int i = 0; i < nums.length; i++) {
+            min = Math.min(nums[i], min);
+            max = Math.max(nums[i], max);
+        }
+        // 计数数组
+        int[] counts = new int[max - min + 1];
+        for (int i = 0; i < nums.length; i++) {
+            counts[nums[i] - min]++;
+        }
+        // 遍历计数数组进行恢复,恢复后默认是排序的
+        System.out.println("temp = " + Arrays.toString(counts));
+        int i = 0;
+        for (int num = min; num <= max; num++) {
+            while (counts[num - min] > 0) {
+                nums[i++] = num;
+                counts[num - min]--;
+            }
+        }
+        return nums;
+    }
+}
