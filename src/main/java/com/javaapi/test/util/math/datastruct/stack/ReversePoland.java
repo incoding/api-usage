c7585fa7fc0729d5110678dc1a4bef44205641f2
王昆 <kun.wang1@ly.com>
2024-04-17 18:59:04 +0800

    中缀表达式与后缀表达式

---
+++ src/main/java/com/javaapi/test/util/math/datastruct/stack/ReversePoland.java
@@ -0,0 +1,14 @@
+package com.javaapi.test.util.math.datastruct.stack;
+
+
+import com.javaapi.test.util.math.leetcode.leetcode.editor.cn.EvaluateReversePolishNotation;
+
+/**
+ * 1 后缀表达式也就是,逆波兰表达式,直接通过栈就能处理
+ *
+ * @see EvaluateReversePolishNotation
+ * 2 中缀表达式转换为后缀表达式也方便处理
+ * https://zq99299.github.io/dsalg-tutorial/dsalg-java-hsp/05/05.html#%E4%B8%80%E4%B8%AA%E7%96%91%E9%97%AE
+ */
+public class ReversePoland {
+}
